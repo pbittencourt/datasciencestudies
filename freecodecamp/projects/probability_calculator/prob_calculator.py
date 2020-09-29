9507d1fc4ed299e7c340838aaf1b4164e0032065
pbittencourt <pedrogabrieltt@gmail.com>
2020-09-28 21:17:26 -0300

    added project probability_calculator

---
+++ freecodecamp/projects/probability_calculator/prob_calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import copy
+import random
+
+class Hat:
+    pass
+
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    pass
+
+if __name__ == "__main__":
+    pass
